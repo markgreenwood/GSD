a68951819f4fd60b1a50120bc0654fd27fa40529
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-05-05 08:34:20 -0700

    First unit test and url mapping, dummy view

--- GSD/urls.py
+++ GSD/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import tasks.views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
+    url(r'^$', tasks.views.home_page, name='home'),
 ]
