a699e294c98a5ce1125af7152a05da7bfd202b02
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-05-06 16:38:22 -0700

    Functional test now checks we can input a task

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,24 +12,33 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Catherine heard about a cool new online GTD app and
-        # goes to check out its homepage
+        # Catherine heard about a cool new online GTD app called
+        # GSD (for Getting Stuff Done) and goes to check out its homepage
         self.browser.get('http://localhost:8000')
         
-        # She notices the page title and header mention GTD
+        # She notices the page title and header mention GSD
         self.assertIn('GSD', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('GSD', header_text)
         
         # She is invited to enter a task right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         
         # She types "Buy groceries" into a text box
+        inputbox.send_keys('Buy groceries')
         
         # When she hits enter, the page updates, and now the page lists 
         # "1: Buy groceries" as an item in a task list
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy groceries' for row in rows))
         
         # There is still a text box inviting her to add another item.
         # She enters "Plan trip to Virginia Tech" (Catherine is a college
         # recruiter for her company)
+        self.fail('Finish the test!')
         
         # The page updates again, and now shows both items on her list
         
