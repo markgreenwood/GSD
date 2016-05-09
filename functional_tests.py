882b1a7a87dcb35aa25252f5310f90505985a79f
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-05-09 10:07:33 -0700

    Refactored FT to use helper fcn to check list for item

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # helper method
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Catherine heard about a cool new online GTD app called
         # GSD (for Getting Stuff Done) and goes to check out its homepage
@@ -31,9 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists 
         # "1: Buy groceries" as an item in a task list
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy groceries', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy groceries')
         
         # There is still a text box inviting her to add another item.
         # She enters "Plan trip to Virginia Tech" (Catherine is a college
@@ -43,11 +47,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy groceries', [row.text for row in rows])
-        self.assertIn('2: Plan trip to Virginia Tech', [row.text for row in
-            rows])
+        self.check_for_row_in_list_table('1: Buy groceries')
+        self.check_for_row_in_list_table('2: Plan trip to Virginia Tech')
         
         # Catherine wonders whether the site will remember her list. Then she
         # sees that the site has generated a unique URL for her -- there is some
