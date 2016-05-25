a8e850328464538e196742971830cd0309afc284
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-05-24 20:12:31 -0700

    3 changed files plus 2 migrations

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item 
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -17,15 +17,23 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html', request=request) # passing request=request handles csrf_token in Django 1.9
         self.assertEqual(response.content.decode(), expected_html)
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "The first (ever) task"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Task the second"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -33,7 +41,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) task')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Task the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -41,8 +51,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='task 1')
-        Item.objects.create(text='task 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='task 1', list=list_)
+        Item.objects.create(text='task 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
