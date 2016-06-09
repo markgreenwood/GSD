b7ad62eeea3ebf865cf82a54c408416484f69b82
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-06-08 20:12:03 -0700

    Hacked FT runner to be able to test staging site

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,23 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -21,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Catherine heard about a cool new online GTD app called
         # GSD (for Getting Stuff Done) and goes to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         
         # She notices the page title and header mention GSD
         self.assertIn('GSD', self.browser.title)
@@ -61,7 +75,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Chrome()
 
         # Thorsten visits the home page. There is no sign of Catherine's list.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy groceries', page_text)
         self.assertNotIn('Plan trip to Virginia Tech', page_text)
@@ -86,7 +100,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Catherine goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
