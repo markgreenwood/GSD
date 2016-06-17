56989b2a7ce9f96b46a16a863a7197e24ccb5ac4
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-06-17 07:24:51 -0700

    Move sqlite database outside of main source tree

--- GSD/settings.py
+++ GSD/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'GSD.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
@@ -119,5 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 STATIC_URL = '/static/'
