cf28f2c75317d132e019064ca0749fcb5923512a
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-06-19 16:15:11 -0700

    Moved FTs into their own individual files

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,19 @@
+from .base import FunctionalTest
+
+class ItemValidationTest(FunctionalTest):
+
+    def test_cannot_add_empty_list_items(self):
+        # Catherine goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+
+        # The home page refreshes, and there is an error message saying that
+        # list items cannot be blank
+
+        # She tries again with some text for the item, which now works
+
+        # Perversely, she now decides to submit a second blank task
+
+        # She receives a similar warning on the task page
+
+        # And she can correct it by filling some text in
+        self.fail('Write this test!')
