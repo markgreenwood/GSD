2449c5640640efc5c752bf7dcdd8414bb8965aff
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-06-24 11:46:55 -0700

    New form to validate against existing list

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+	ItemForm, ExistingListItemForm,
+	EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
+)
 from lists.models import Item, List
 
 class ItemFormTest(TestCase):
+
 	def test_form_item_input_has_placeholder_and_css_classes(self):
 		form = ItemForm()
 		self.assertIn('placeholder="Enter a task"', form.as_p())
@@ -23,3 +27,29 @@ class ItemFormTest(TestCase):
 		self.assertEqual(new_item, Item.objects.first())
 		self.assertEqual(new_item.text, 'do me')
 		self.assertEqual(new_item.list, list_)
+
+class ExistingListItemFormTest(TestCase):
+
+	def test_form_renders_item_text_input(self):
+		list_ = List.objects.create()
+		form = ExistingListItemForm(for_list=list_)
+		self.assertIn('placeholder="Enter a task"', form.as_p())
+
+	def test_form_validation_for_blank_items(self):
+		list_ = List.objects.create()
+		form = ExistingListItemForm(for_list=list_, data={'text': ''})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(
+			form.errors['text'],
+			[EMPTY_ITEM_ERROR]
+		)
+
+	def test_form_validation_for_duplicate_items(self):
+		list_ = List.objects.create()
+		Item.objects.create(list=list_, text='no twins!')
+		form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(
+			form.errors['text'],
+			[DUPLICATE_ITEM_ERROR]
+		)
